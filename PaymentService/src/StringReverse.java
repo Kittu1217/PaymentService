b99c5fa0aec8a4886de5365c30e6dbb6a65d8523
Kishore <kishorebabu.ganta@gmail.com>
2025-09-07 19:09:23 +0530

    new file added

---
+++ PaymentService/src/StringReverse.java
@@ -0,0 +1,10 @@
+public class StringReverse {
+    public static String reverse(String input){
+        String reverse="";
+
+        for (int i = input.length()-1; i >=0 ; i--) {
+            reverse=reverse+input.charAt(i);
+        }
+        return reverse;
+    }
+}
