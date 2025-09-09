b54e6e91c247f4b005ef012cbf2e883ba9d3bf86
Kishore <kishorebabu.ganta@gmail.com>
2025-09-09 21:55:02 +0530

    new logic added

--- PaymentService/src/Main.java
+++ PaymentService/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
@@ -7,5 +9,10 @@ public class Main {
         String reverse=StringReverse.reverse("kishore");
         System.out.println(reverse);
         System.out.println("============");
+        int[] a={5,6,1,3,2,4,7};
+        System.out.println(Arrays.toString(a));
+        SortArray.sortArr(a);
+        System.out.println(Arrays.toString(a));
+
     }
 }
