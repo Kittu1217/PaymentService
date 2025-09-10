a8b511b7bb96cd5643d15831ae319078c3cc8987
Kishore <50565866+Kittu1217@users.noreply.github.com>
2025-09-10 18:50:19 +0530

    Update Main.java

--- PaymentService/src/Main.java
+++ PaymentService/src/Main.java
@@ -2,10 +2,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Welcome");
-        System.out.println("Github");
-
         String reverse=StringReverse.reverse("kishore");
         System.out.println(reverse);
         int[] a={5,6,1,3,2,4,7};
